c730a85ca35350f2a38c9f510434627b1437b699
Diego G. Dreossi <dididreo@hotmail.com>
2022-03-08 19:06:17 -0300

    projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsfilme/DsfilmeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsfilmeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsfilme/DsfilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
