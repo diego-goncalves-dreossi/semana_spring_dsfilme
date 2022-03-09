bb3a7a4d4cbd28c21e11cc104d6d8b416003d9a0
Diego G. Dreossi <dididreo@hotmail.com>
2022-03-08 21:21:37 -0300

    bootstrap classes

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,7 +7,13 @@ function Listing(){
         <Pagination />
         <div className="container">
             <div className="row">
-                <div className="col">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                     <MovieCard />
                 </div>
             </div>
