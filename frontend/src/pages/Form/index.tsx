61324de02c0ef396f1a7df2542b9eea043a3e7b4
Diego G. Dreossi <dididreo@hotmail.com>
2022-03-12 18:55:51 -0300

    useParams

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,42 +1,15 @@
-import './estilos.css';
-import {Link} from "react-router-dom"
+import { useParams } from "react-router-dom";
+import FormCard from "../../components/FormCard";
+
+
+
 
 function Form() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+
+    const params = useParams();
 
     return (
-        <div className="dsfilme-form-container">
-            <img className="dsfilme-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsfilme-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <form className="dsfilme-form">
-                    <div className="form-group dsfilme-form-group">
-                        <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group dsfilme-form-group">
-                        <label htmlFor="score">Informe sua avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div className="dsfilme-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsfilme-btn">Salvar</button>
-                    </div>
-                </form >
-                <Link to='/'><button className="btn btn-primary dsfilme-btn mt-3">Cancelar</button></Link>
-            </div >
-        </div >
+        <FormCard movieId={`${params.movieId}`} />
     );
 }
 
